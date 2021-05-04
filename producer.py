b8a0f7d92f778ea8995d6fec64ce358ac498e9fe
vincentwengert <vincent.wengert@web.de>
2021-05-04 23:03:52 +0200

    optimized logging

--- producer.py
+++ producer.py
@@ -6,8 +6,7 @@ from datetime import datetime, timedelta
 from newsapi import NewsApiClient
 
 # Init Logging
-logging.basicConfig(filename='producer.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
+logging.basicConfig(filename='producer.log')
 
 # Init news api access
 newsapi = NewsApiClient(api_key='570256fa98ba4955b0a3b160fa5dd99f')
@@ -16,7 +15,7 @@ newsapi = NewsApiClient(api_key='570256fa98ba4955b0a3b160fa5dd99f')
 from_date = datetime.now() - timedelta(hours=4)  # Subtract 1 day from To Date..1 Week
 to_date = datetime.now()  # to Date
 print('Articles are pulled between', from_date, 'and', to_date)
-logging.info('Articles are pulled between', from_date, 'and', to_date)
+logging.info('date=%s', ('Articles are pulled between', from_date, 'and', to_date))
 
 # Keys to remove from result
 keys_to_remove = ["urlToImage", "url", "description", "content"]
@@ -29,7 +28,7 @@ all_articles = newsapi.get_everything(q='biden',  # topic
                                       page_size=100)  # language
 
 print('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults'])
-logging.info('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults'])
+logging.info('date=%s', ('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults']))
 
 # Remove irrelevant keys from result
 for i in all_articles["articles"]:
@@ -52,7 +51,7 @@ if __name__ == "__main__":
     # Create producer
     producer = Producer(conf)
     for i in all_articles["articles"]:
-        logging.info(i)
+        logging.info('date=%s', i)
         producer.produce("articles", json.dumps(i, indent=4), callback=delivery_report)
 
     producer.flush()
