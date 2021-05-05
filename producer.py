ea60d41faa466fb5e5a8391d37893cae6e537a05
vincentwengert <vincent.wengert@web.de>
2021-05-05 15:29:08 +0200

    added logic for retrieving articles in past

--- producer.py
+++ producer.py
@@ -11,29 +11,15 @@ logging.basicConfig(filename='producer.log')
 # Init news api access
 newsapi = NewsApiClient(api_key='570256fa98ba4955b0a3b160fa5dd99f')
 
-# date variables
-from_date = datetime.now() - timedelta(hours=4)  # Subtract 1 day from To Date..1 Week
+# Date and time variables
+timedelta_requests = 1  # request span
+from_date = datetime.now() - timedelta(hours=timedelta_requests)  #
 to_date = datetime.now()  # to Date
-print('Articles are pulled between', from_date, 'and', to_date)
-logging.info('date=%s', ('Articles are pulled between', from_date, 'and', to_date))
+hours_past = 100  # hours to go in past to retrieve past data
+retrieve_past_articles = 0  # 1 = true, 0 = false
 
 # Keys to remove from result
 keys_to_remove = ["urlToImage", "url", "description", "content"]
-
-# Request for retrieving articles
-all_articles = newsapi.get_everything(q='biden',  # topic
-                                      from_param=from_date,  # start of interval
-                                      to=to_date,
-                                      language='en',
-                                      page_size=100)  # language
-
-print('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults'])
-logging.info('date=%s', ('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults']))
-
-# Remove irrelevant keys from result
-for i in all_articles["articles"]:
-    for key in keys_to_remove:
-                del i[key]
 
 
 def delivery_report(err, msg):
@@ -50,8 +36,48 @@ if __name__ == "__main__":
 
     # Create producer
     producer = Producer(conf)
-    for i in all_articles["articles"]:
-        logging.info('date=%s', i)
-        producer.produce("articles", json.dumps(i, indent=4), callback=delivery_report)
+
+    if retrieve_past_articles == 1:
+        # Request for retrieving past articles
+        for i in range(0, hours_past):
+            all_articles = newsapi.get_everything(q='biden',  # topic
+                                                  from_param=from_date,  # start of interval
+                                                  to=to_date,
+                                                  language='en',
+                                                  page_size=100)  # language
+            from_date = from_date - timedelta(hours=1)
+            to_date = to_date - timedelta(hours=1)
+            print('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults'])
+
+            # Remove irrelevant keys from result
+            for i in all_articles["articles"]:
+                for key in keys_to_remove:
+                    del i[key]
+
+            for i in all_articles["articles"]:
+                print(i)
+                logging.info('date=%s', i)
+                producer.produce("articles", json.dumps(i, indent=4), callback=delivery_report)
+
+    else:
+        all_articles = newsapi.get_everything(q='biden',  # topic
+                                              from_param=from_date,  # start of interval
+                                              to=to_date,
+                                              language='en',
+                                              page_size=100)  # language
+        print('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults'])
+
+        # Remove irrelevant keys from result
+        for i in all_articles["articles"]:
+            for key in keys_to_remove:
+                del i[key]
+
+        for i in all_articles["articles"]:
+            print(i)
+            logging.info('date=%s', i)
+            producer.produce("articles", json.dumps(i, indent=4), callback=delivery_report)
+
+        logging.info('date=%s',
+                     ('Retrieved Articles between', from_date, 'and', to_date, '=', all_articles['totalResults']))
 
     producer.flush()
